75ef6bafee5b3ad3ca15f63ec805e2bc87794cac
Marcos Vinicius Borges <marcos.borges@hmv.org.br>
2023-02-01 11:46:10 -0300

    Refs create arquitetura Base

---
+++ src/arquiteturaBase.ioc/ServiceRegistry.cs
@@ -0,0 +1,15 @@
+﻿using StructureMap.Attributes;
+using StructureMap.Configuration.DSL;
+
+namespace arquiteturaBase.ioc
+{
+    public class ServiceRegistry : Registry
+    {
+        public ServiceRegistry()
+        {
+            //ForRequestedType<IService>()
+            //    .CacheBy(InstanceScope.PerRequest)
+            //    .TheDefault.Is.OfConcreteType<Service>();
+        }
+    }
+}
