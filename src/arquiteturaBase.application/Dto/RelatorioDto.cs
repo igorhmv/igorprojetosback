f16459b7e7ac9c893cb7b3e0bb2f0ea64ec6935b
igorhmv <125670751+igorhmv@users.noreply.github.com>
2023-02-22 11:02:18 -0300

    Add files via upload

--- src/arquiteturaBase.application/Dto/RelatorioDto.cs
+++ src/arquiteturaBase.application/Dto/RelatorioDto.cs
@@ -9,7 +9,12 @@ namespace arquiteturaBase.application.Dto
     public class RelatorioDto
     {
         public string nm_paciente { get; set; }
-        
+        public string nm_convenio { get; set; }
+        public string cd_atendimento { get; set; }
+        public string cd_reg_fat { get; set; }
+        public string cd_reg_amb { get; set; }
+        public string cd_gru_fat { get; set; }
+        public string cd_pro_fat { get; set; }
         public string dt_auditoria { get; set; }
         
 
