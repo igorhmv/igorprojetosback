2c385299774d08b016b6542e4efb78f97c1e3a50
igorhmv <125670751+igorhmv@users.noreply.github.com>
2023-02-23 09:28:20 -0300

    Add files via upload

--- src/arquiteturaBase.application/Dto/RelatorioDto.cs
+++ src/arquiteturaBase.application/Dto/RelatorioDto.cs
@@ -15,10 +15,27 @@ namespace arquiteturaBase.application.Dto
         public string cd_reg_amb { get; set; }
         public string cd_gru_fat { get; set; }
         public string cd_pro_fat { get; set; }
-        public string dt_auditoria { get; set; }
+        public string ds_pro_fat { get; set; }
+        public string ds_motivo_auditoria { get; set; }
         
+        
+        public string tipo_auditoria { get; set; }
+        public string nm_setor { get; set; }
+        public int qt_lancamento_ant { get; set; }
+        public int qt_lancamento { get; set; }
+        public int qt_total_ajustado { get; set; }
+        
+        public double vl_total_conta_ant { get; set; }
+        public double vl_total_conta { get; set; }
+        public double exclusaoxinclusao { get; set; }
 
+        public string nm_usuario { get; set; }
+        public string cd_usuario { get; set; }
+        public string setor_usuario { get; set; }
 
+        public string dt_auditoria { get; set; }
+
+        public string acao { get; set; }
 
     }
 }
