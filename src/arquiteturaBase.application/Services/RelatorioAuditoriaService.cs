63b14575226dd57a4ceeff8ea369a17518f4933f
igorhmv <125670751+igorhmv@users.noreply.github.com>
2023-02-23 09:28:53 -0300

    Add files via upload

--- src/arquiteturaBase.application/Services/RelatorioAuditoriaService.cs
+++ src/arquiteturaBase.application/Services/RelatorioAuditoriaService.cs
@@ -19,13 +19,7 @@ namespace arquiteturaBase.application.Services
         {
             StringBuilder qry = new StringBuilder();
 
-            qry.AppendFormat(@"SELECT vw.nm_paciente, 
-                                vw.dt_auditoria,
-                                vw.cd_atendimento,
-                                vw.cd_reg_fat,
-                                vw.cd_reg_amb,
-                                vw.cd_gru_fat,
-                                vw.cd_pro_fat
+            qry.AppendFormat(@"SELECT *
                                 FROM dbahmv.vw_auditoria_operacional vw
                                 WHERE vw.dt_auditoria >= to_date('{0}','DD/MM/YYYY') 
                                 AND vw.dt_auditoria <= to_date('{1}','DD/MM/YYYY')", 
@@ -33,12 +27,26 @@ namespace arquiteturaBase.application.Services
 
             IQuery query = ObjectFactory.GetInstance<IUnitOfWork>().CurrentSession.CreateSQLQuery(qry.ToString())     
                      .AddScalar("nm_paciente", NHibernateUtil.String)
-                     .AddScalar("dt_auditoria", NHibernateUtil.String)
                      .AddScalar("cd_atendimento", NHibernateUtil.String)
                      .AddScalar("cd_reg_fat", NHibernateUtil.String)
                      .AddScalar("cd_reg_amb", NHibernateUtil.String)
                       .AddScalar("cd_gru_fat", NHibernateUtil.String)
                        .AddScalar("cd_pro_fat", NHibernateUtil.String)
+                       .AddScalar("ds_pro_fat", NHibernateUtil.String)
+                       .AddScalar("ds_motivo_auditoria", NHibernateUtil.String)
+                       .AddScalar("tipo_auditoria", NHibernateUtil.String)
+                       .AddScalar("nm_setor", NHibernateUtil.String)
+                       .AddScalar("qt_lancamento_ant",NHibernateUtil.Int32)
+                       .AddScalar("qt_lancamento", NHibernateUtil.Int32)
+                       .AddScalar("qt_total_ajustado", NHibernateUtil.Int32)
+                       .AddScalar("vl_total_conta_ant",NHibernateUtil.Double)
+                       .AddScalar("vl_total_conta", NHibernateUtil.Double)
+                       .AddScalar("exclusaoxinclusao", NHibernateUtil.Double)
+                       .AddScalar("nm_usuario", NHibernateUtil.String)
+                       .AddScalar("cd_usuario", NHibernateUtil.String)
+                       .AddScalar("setor_usuario", NHibernateUtil.String)
+                       .AddScalar("dt_auditoria", NHibernateUtil.String)
+                       .AddScalar("acao", NHibernateUtil.String)
                      ;
 
             query.SetResultTransformer(Transformers.AliasToBean(typeof(RelatorioDto)));
